23cd1550766b4910016f6b10b817ec2668552b88
Alvaro297 <alvaromartinfalagan29@gmail.com>
2024-12-02 23:14:37 +0100

    Avanzado

--- Headers/libft.h
+++ Headers/libft.h
@@ -22,7 +22,7 @@ char    *ft_strncat(char *s1, const char *s2, size_t n);
 size_t  ft_strlcat(char *dst, const char *src, size_t dstsize);
 char    *ft_strchr(const char *s, int c);
 char    *ft_strrchr(const char *s, int c);
-char    *ft_strstr(const char *haystack, const char *needle);
+char	*strstr(const char *s1, const char *s2);
 char    *ft_strnstr(const char *haystack, const char *needle, size_t len);
 int     ft_strcmp(const char *s1, const char *s2);
 int     ft_strncmp(const char *s1, const char *s2, size_t n);
