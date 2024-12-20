06d8996329cb973ae724c5a178137f32d5a656e9
Alvaro Martin Falagan <alvamart@c2r16s4.42madrid.com>
2024-12-20 17:28:15 +0100

    Fuera de leaks

--- Rendering/render_map.c
+++ Rendering/render_map.c
@@ -34,6 +34,7 @@ void	ft_move(t_mlx *mlx, t_player player, int key, void *img_background)
 		mlx->player.y += 1;
 	if (mlx->map.matriz[mlx->player.x][mlx->player.y] == 'C')
 		ft_delete_collect(mlx);
+	mlx_destroy_image(mlx->mlx_ptr, img_background);
 	ft_validate_win_game(mlx, key);
 	ft_change_player_texture(mlx, player, key);
 	mlx_do_sync(mlx->mlx_ptr);
@@ -54,10 +55,7 @@ int	ft_key_hook(int keycode, t_mlx *mlx)
 	}
 	else if (keycode == KY_W || keycode == KY_S
 		|| keycode == KY_A || keycode == KY_D)
-		{
-			mlx_destroy_image(mlx->mlx_ptr, img_background);
 			ft_move(mlx, mlx->player, keycode, img_background);
-		}
 	return (0);
 }
 
